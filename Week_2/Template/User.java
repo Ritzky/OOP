19fec2b21af121c1e3baaa6ca0c268b7e8b94e0b
Ritz <35006014+Ritzky@users.noreply.github.com>
2022-02-01 10:24:02 +0000

    Create User.java

---
+++ Week_2/Template/User.java
@@ -0,0 +1,17 @@
+//each user in the market
+public class User{
+
+    private String name;
+    
+    public User(String name){
+	this.name = name;
+    }
+    
+    public void setName(String name){
+	this.name = name;
+    }
+
+    public String getName(){
+	return this.name;
+    }    
+}
